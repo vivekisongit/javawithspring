c1ae3000a7b5da8ba358a6eca3596c22b31a003c
Vivek Kumar <Vivek.Kumar3@Ltts.com>
2020-12-02 15:32:25 +0530

    kubernates installsetps pushed

--- vkrstore/src/main/java/com/vkr/store/controllers/HomeController.java
+++ vkrstore/src/main/java/com/vkr/store/controllers/HomeController.java
@@ -3,36 +3,43 @@ package com.vkr.store.controllers;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.vkr.store.services.ServiceInfo;
 
+
+
+
+
 @Controller
 public class HomeController {
-
-	@RequestMapping("/")	
-	public String home(HashMap<String,Object> model) {
-		
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String home() {		
+		return "Hello World!!!";	
+	}
+	
+	@RequestMapping("/home")	
+	public String home(HashMap<String,Object> model) {		
 		List<ServiceInfo> services= Arrays.asList(
 				new ServiceInfo("Service List", "/services")				
-				);
-		
+				);		
 		model.put("services", services);
 		//path of the jsp file
 		return "/WEB-INF/views/index.jsp";
-	}
-	
+	}	
 	
 	@RequestMapping("/welcome")
 	@ResponseBody
 	public String welcome() {
 		
-		return "Hello World, Welcome to Spring Boot";
+		return "Welcome to Spring Boot";
 	
 	}
+	
 	@RequestMapping("/services")
 	@ResponseBody
 	public String services() {
